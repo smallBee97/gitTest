2f1520540a1f942854c9fcbb74727b4eeba35cf9
hcf <hcf@atguigu.com>
2021-08-14 15:42:14 +0800

    my first commit by idea

---
+++ src/main/java/com/atguigu/exec/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.atguigu.exec;
+
+/**
+ * @author hcf
+ * @create 2021-08-14 15:40
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
